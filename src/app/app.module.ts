46a9fa34910280eec6ecf84b9b9a646644d8ecc3
ixaxion <lenell.davis@gmail.com>
2018-09-21 19:06:34 +0700

    Update 1-ionic pro config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,35 @@ import {AngularFireAuthModule} from 'angularfire2/auth';
 
 firebase.initializeApp(FIREBASE_CREDENTIALS);
 
+// These are all imports required for Pro Client with Monitoring & Deploy,
+// feel free to merge into existing imports above.
+import { Pro } from '@ionic/pro';
+
+Pro.init('e2663aac', {
+  appVersion: '0.0.1'
+})
+
+@Injectable()
+export class MyErrorHandler implements ErrorHandler {
+  ionicErrorHandler: IonicErrorHandler;
+
+  constructor(injector: Injector) {
+    try {
+      this.ionicErrorHandler = injector.get(IonicErrorHandler);
+    } catch(e) {
+      // Unable to get the IonicErrorHandler provider, ensure
+      // IonicErrorHandler has been added to the providers list below
+    }
+  }
+
+  handleError(err: any): void {
+    Pro.monitoring.handleNewError(err);
+    // Remove this if you want to disable Ionic's auto exception handling
+    // in development mode.
+    this.ionicErrorHandler && this.ionicErrorHandler.handleError(err);
+  }
+}
+
 
 @NgModule({
   declarations: [
