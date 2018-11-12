8bbb80c31f3092ebdc0427ffb56a49be9a8f12a5
Lenell Davis <emoxycilin@gmail.com>
2018-11-12 12:44:00 -0500

    Deleted keys

--- src/firebase.credentials.ts
+++ src/firebase.credentials.ts
@@ -1,8 +1,8 @@
 export const FIREBASE_CREDENTIALS =  {
-    apiKey: "AIzaSyD53ColPIL4IrWCQAaNithhX6vtLqcCgUs",
-    authDomain: "inventory-app-2c264.firebaseapp.com",
-    databaseURL: "https://inventory-app-2c264.firebaseio.com",
-    projectId: "inventory-app-2c264",
-    storageBucket: "inventory-app-2c264.appspot.com",
-    messagingSenderId: "55927647314"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   };
